b86888e5d6e9df94bb9f378f00181cbf9533827f
AnirudhVasudev47 <69135996+AnirudhVasudev47@users.noreply.github.com>
2021-04-08 11:09:18 +0530

    Auto indent.

--- lib/main_tabs/home_tab.dart
+++ lib/main_tabs/home_tab.dart
@@ -12,11 +12,9 @@ class HomeTab extends StatefulWidget {
 }
 
 class _HomeTabState extends State<HomeTab> {
-
   final int milli = 50;
   ScrollController controller = ScrollController();
   bool shrinkAmount = false;
-  Curve _curve = Curves.easeIn;
 
   @override
   void initState() {
@@ -32,7 +30,6 @@ class _HomeTabState extends State<HomeTab> {
 
   @override
   Widget build(BuildContext context) {
-
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10),
       child: Column(
@@ -42,41 +39,88 @@ class _HomeTabState extends State<HomeTab> {
             size: Size.square(60),
             image: 'images/face.png',
             opacity: 1,
-            onPressed: (){
-            Navigator.push(
+            onPressed: () {
+              Navigator.push(
                 context,
                 ScaleRoute(page: UserProfileScreen()),
               );
             },
           ),
-          AmountInAccount(floatFontSize: shrinkAmount?10:20, fontSize: shrinkAmount?20:40,),
+          AmountInAccount(
+            floatFontSize: shrinkAmount ? 10 : 20,
+            fontSize: shrinkAmount ? 20 : 40,
+          ),
           Expanded(
             child: ListView(
               controller: controller,
               children: <Widget>[
-                DaySeperator(dayName: 'Today', seeAll: true, onPressed: (){
-                  Navigator.push(
-                    context,
-                      MaterialPageRoute(builder: (context) => MonthWiseTransactionsScreen()),
-                  );
-                },),
-                TransactionListCard(image: 'images/uber.png', title: 'Uber', date: '10:00am / Mar 12', amount: '107.00', piggyAmount: '3',),
-
-                TransactionListCard(image: 'images/user_x.png', title: 'Transfer from User X', date: '3:20pm / Mar 12', color: Color(0xff53D07D), amount: '5,000.00',),
-
-                TransactionListCard(image: 'images/netflix.png', title: 'Netflix', date: '3:20pm / Mar 12', amount: '299.00',),
-
-                TransactionListCard(image: 'images/user_x.png', title: 'Transfer to Goa Saver', date: '4:20pm / Mar 12', amount: '3000.00',),
-
-                DaySeperator(dayName: 'Yesterday', seeAll: false,),
-                TransactionListCard(image: 'images/amazon.png', title: 'Prime', date: '10:00am / Mar 11', amount: '999.00', piggyAmount: '1',),
-
-                TransactionListCard(image: 'images/user_x.png', title: 'Transfer to User Y', date: '3:20pm / Mar 11', color: Color(0xff53D07D), amount: '3,500.00',),
-
-                TransactionListCard(image: 'images/netflix.png', title: 'Netflix', date: '3:20pm / Mar 11', amount: '299.00',),
-
-                TransactionListCard(image: 'images/user_x.png', title: 'Transfer to Goa Saver', date: '4:20pm / Mar 11', amount: '3000.00',),
-
+                DaySeperator(
+                  dayName: 'Today',
+                  seeAll: true,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MonthWiseTransactionsScreen()),
+                    );
+                  },
+                ),
+                TransactionListCard(
+                  image: 'images/uber.png',
+                  title: 'Uber',
+                  date: '10:00am / Mar 12',
+                  amount: '107.00',
+                  piggyAmount: '3',
+                ),
+                TransactionListCard(
+                  image: 'images/user_x.png',
+                  title: 'Transfer from User X',
+                  date: '3:20pm / Mar 12',
+                  color: Color(0xff53D07D),
+                  amount: '5,000.00',
+                ),
+                TransactionListCard(
+                  image: 'images/netflix.png',
+                  title: 'Netflix',
+                  date: '3:20pm / Mar 12',
+                  amount: '299.00',
+                ),
+                TransactionListCard(
+                  image: 'images/user_x.png',
+                  title: 'Transfer to Goa Saver',
+                  date: '4:20pm / Mar 12',
+                  amount: '3000.00',
+                ),
+                DaySeperator(
+                  dayName: 'Yesterday',
+                  seeAll: false,
+                ),
+                TransactionListCard(
+                  image: 'images/amazon.png',
+                  title: 'Prime',
+                  date: '10:00am / Mar 11',
+                  amount: '999.00',
+                  piggyAmount: '1',
+                ),
+                TransactionListCard(
+                  image: 'images/user_x.png',
+                  title: 'Transfer to User Y',
+                  date: '3:20pm / Mar 11',
+                  color: Color(0xff53D07D),
+                  amount: '3,500.00',
+                ),
+                TransactionListCard(
+                  image: 'images/netflix.png',
+                  title: 'Netflix',
+                  date: '3:20pm / Mar 11',
+                  amount: '299.00',
+                ),
+                TransactionListCard(
+                  image: 'images/user_x.png',
+                  title: 'Transfer to Goa Saver',
+                  date: '4:20pm / Mar 11',
+                  amount: '3000.00',
+                ),
               ],
             ),
           ),
@@ -88,31 +132,32 @@ class _HomeTabState extends State<HomeTab> {
 
 class ScaleRoute extends PageRouteBuilder {
   final Widget page;
+
   ScaleRoute({this.page})
       : super(
-    pageBuilder: (
-        BuildContext context,
-        Animation<double> animation,
-        Animation<double> secondaryAnimation,
-        ) =>
-    page,
-    transitionsBuilder: (
-        BuildContext context,
-        Animation<double> animation,
-        Animation<double> secondaryAnimation,
-        Widget child,
-        ) =>
-        ScaleTransition(
-          scale: Tween<double>(
-            begin: 0.0,
-            end: 1.0,
-          ).animate(
-            CurvedAnimation(
-              parent: animation,
-              curve: Curves.easeIn,
+          pageBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+          ) =>
+              page,
+          transitionsBuilder: (
+            BuildContext context,
+            Animation<double> animation,
+            Animation<double> secondaryAnimation,
+            Widget child,
+          ) =>
+              ScaleTransition(
+            scale: Tween<double>(
+              begin: 0.0,
+              end: 1.0,
+            ).animate(
+              CurvedAnimation(
+                parent: animation,
+                curve: Curves.easeIn,
+              ),
             ),
+            child: child,
           ),
-          child: child,
-        ),
-  );
+        );
 }
